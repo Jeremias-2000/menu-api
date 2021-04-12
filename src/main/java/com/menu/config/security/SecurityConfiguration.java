21d0e535a0643217181832ef2b0ecda67abed71f
Jeremias <jdossantospinheiro20@gmail.com>
2021-04-12 14:49:11 -0300

    adicionando spring security

---
+++ src/main/java/com/menu/config/security/SecurityConfiguration.java
@@ -0,0 +1,49 @@
+package com.menu.config.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/","index","/css/*","/js/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+      UserDetails jeremias =  User.builder()
+                .username("jeremias")
+                .password(passwordEncoder.encode("password"))
+                .roles("CUSTOMER")// role_customer
+                .build();
+
+        return new InMemoryUserDetailsManager(jeremias);
+    }
+}
--- src/main/java/com/menu/document/Food.java
+++ src/main/java/com/menu/document/Food.java
@@ -22,6 +22,7 @@ import javax.validation.constraints.Size;
 @EqualsAndHashCode
 
 public class Food {
+
     @Id
     private String id;
 
